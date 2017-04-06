dfdac5b759825e5d1e40d448b2c10a0b8f9bdc18
smemons <shoukatmemon@Shoukats-MacBook-Pro.local>
2017-04-06 09:12:05 +0300

    test

--- src/app/activity/addActivity/activity.component.ts
+++ src/app/activity/addActivity/activity.component.ts
@@ -6,7 +6,7 @@ import { Userservice } from './../../services/userservice.service';
 import { Category } from './../../models/category';
 import { User } from './../../models/user';
 import { forEach } from '@angular/router/src/utils/collection';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import {SelectItem} from 'primeng/primeng';
 
@@ -31,7 +31,8 @@ export class ActivityComponent implements OnInit {
               private utilityService:UtilityService,
               private alertService:AlertService,
               private activityService:ActivityService,
-              private router:Router ) { }
+              private router:Router,
+              private cd: ChangeDetectorRef ) { }
 
   ngOnInit() {
     // //get all the dept List
@@ -40,7 +41,7 @@ export class ActivityComponent implements OnInit {
 
       //get all the users and create
        depts.forEach(dept=> {
-
+          this.cd.detectChanges();
             this.depts.push({label:dept.title, value:dept._id});
        });
     });
@@ -52,8 +53,8 @@ export class ActivityComponent implements OnInit {
   //get all Status
 this.statuses=[];
     this.utilityService.getAllStatus().subscribe(sts=>{
+    this.cd.detectChanges();
     sts.forEach(status => {
-
       this.statuses.push({label:status.title, value:status._id});
     });
   })
@@ -61,6 +62,7 @@ this.statuses=[];
    //get all focus areas
 this.focuses=[];
     this.utilityService.getAllFocuses().subscribe(foc=>{
+       this.cd.detectChanges();
     foc.forEach(focus => {
 
       this.focuses.push({label:focus.title, value:focus._id});
@@ -71,6 +73,7 @@ this.focuses=[];
    //get all visiblities areas
 this.visibilities=[];
     this.utilityService.getAllVisibilities().subscribe(vis=>{
+       this.cd.detectChanges();
     vis.forEach(visb => {
 
       this.visibilities.push({label:visb.title, value:visb._id});
@@ -80,6 +83,7 @@ this.visibilities=[];
    //get all phases areas
 this.phases=[];
     this.utilityService.getAllPhases().subscribe(phase=>{
+       this.cd.detectChanges();
     phase.forEach(ph => {
 
       this.phases.push({label:ph.title, value:ph._id});
